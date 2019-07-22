c71875b9939d4db2819b6d942cd777f25d20c92b
Gregory <hommertg@gmail.com>
2019-07-21 21:12:21 -0500

    Content Added

---
+++ ExtraCredit2/Service/AbilityServiceClient.swift
@@ -0,0 +1,103 @@
+import Foundation
+
+#warning("Problems 5-8")
+
+final class AbilityServiceClient {
+    private let baseServceClient: BaseServiceClient
+    private let urlProvider: UrlProvider
+    
+    init(baseServceClient: BaseServiceClient, urlProvider: UrlProvider) {
+        self.baseServceClient = baseServceClient
+        self.urlProvider = urlProvider
+    }
+}
+
+/*** 5 ***/
+
+struct AbilityList: Decodable {
+    let items: [URL]              //let results: [NameUrlPair]
+    
+    // Write CodingKeys and custom init(from decoder: Decoder) here
+}
+
+struct Ability: Decodable {
+    let id: Int
+    let name: String
+    let isMainSeries: Bool
+    let generation: String          //NameUrlPair
+    let names: [Name]
+    let effectEntries: [EffectEntry]
+    let effectChanges: [EffectChange]
+    let flavorTextEntries: [FlavorTextEntry]
+    let pokemon: [PokemonForAbility]
+    
+    // Write CodingKeys and custom init(from decoder: Decoder) here
+}
+
+struct Name: Decodable {
+    let language: String            //NameUrlPair
+    let value: String               //let name: String
+    
+    // Write CodingKeys and custom init(from decoder: Decoder) here
+}
+
+struct EffectEntry: Decodable {
+    let effect: String
+    let language: String            //NameUrlPair
+    let shortEffect: String?        // HINT: - decodeIfPresent
+    
+    // Write CodingKeys and custom init(from decoder: Decoder) here
+}
+
+struct EffectChange: Decodable {
+    let effectEntries: [EffectEntry]
+    let versionGroup: String        //NameUrlPair
+    
+    // Write CodingKeys and custom init(from decoder: Decoder) here
+}
+
+struct FlavorTextEntry: Decodable {
+    let flavorText: String
+    let language: String            //NameUrlPair
+    let versionGroup: String        //NameUrlPair
+    
+    // Write CodingKeys and custom init(from decoder: Decoder) here
+}
+
+struct PokemonForAbility: Decodable {
+    let isHidden: Bool
+    let pokemon: String             //NameUrlPair
+    let slot: Int
+    
+    // Write CodingKeys and custom init(from decoder: Decoder) here
+}
+
+/*** 6 ***/
+
+// Complete result typealias' here
+//typealias AbilityListResult =
+//typealias AbilityResult =
+
+extension AbilityServiceClient {
+    /*** 7 ***/
+    
+    // Complete problem 6 before beginning this section so that the real function signature may be un-commented
+    func getAbilityList() {//(completion: @escaping (AbilityListResult) ->()) {
+        let pathComponents = ["ability"]
+        let parameters = ["offset": "\(0)", "limit": "\(293)"]
+        let url = urlProvider.url(forPathComponents: pathComponents, parameters: parameters)
+        
+        // Write function body here
+    }
+    
+    /*** 8 ***/
+    
+    // Complete problem 6 before beginning this section so that the real function signature may be un-commented
+    func getAbility(id: Int) {//(id: Int, completion: @escaping (AbilityResult) ->()) {
+        let pathComponents = ["ability", "\(id)"]
+        let parameters: [String: String] = [:]
+        let url = urlProvider.url(forPathComponents: pathComponents, parameters: parameters)
+        
+        // Write function body here
+    }
+}
---
+++ ExtraCredit2/Service/BerryServiceClient.swift
@@ -0,0 +1,74 @@
+import Foundation
+
+#warning("Problems 1-4")
+
+final class BerryServiceClient {
+    private let baseServceClient: BaseServiceClient
+    private let urlProvider: UrlProvider
+    
+    init(baseServceClient: BaseServiceClient, urlProvider: UrlProvider) {
+        self.baseServceClient = baseServceClient
+        self.urlProvider = urlProvider
+    }
+}
+
+/*** 1 ***/
+
+struct BerryList: Decodable {
+    let items: [URL]              //let results: [NameUrlPair]
+    
+    // Write CodingKeys and custom init(from decoder: Decoder) here
+}
+
+struct Berry: Decodable {
+    let id: Int
+    let growthTime: Int
+    let maxHarvest: Int
+    let naturalGiftPower: Int
+    let size: Int
+    let smoothness: Int
+    let soilDryness: Int
+    let firmness: String            //NameUrlPair
+    let flavors: [BerryFlavorMap]
+    let item: String                //NameUrlPair
+    let naturalGiftType: String     //NameUrlPair
+    
+    // Write CodingKeys and custom init(from decoder: Decoder) here
+}
+
+struct BerryFlavorMap: Decodable {
+    let potency: Int
+    let flavor: String              //NameUrlPair
+    
+    // Write CodingKeys and custom init(from decoder: Decoder) here
+}
+
+/*** 2 ***/
+
+// Complete result typealias' here
+//typealias BerryListResult =
+//typealias BerryResult =
+
+extension BerryServiceClient {
+    /*** 3 ***/
+    
+    // Complete problem 2 before beginning this section so that the real function signature may be un-commented
+    func getBerryList() {//(completion: @escaping (BerryListResult) ->()) {
+        let pathComponents = ["berry"]
+        let parameters = ["offset": "\(0)", "limit": "\(64)"]
+        let url = urlProvider.url(forPathComponents: pathComponents, parameters: parameters)
+        
+        // Write function body here
+    }
+    
+    /*** 4 ***/
+    
+    // Complete problem 2 before beginning this section so that the real function signature may be un-commented
+    func getBerry(id: Int) {//(id: Int, completion: @escaping (BerryResult) ->()) {
+        let pathComponents = ["berry", "\(id)"]
+        let parameters: [String: String] = [:]
+        let url = urlProvider.url(forPathComponents: pathComponents, parameters: parameters)
+        
+        // Write function body here
+    }
+}
